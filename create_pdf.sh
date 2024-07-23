fb5d847f803102aa7e61ed4336af6c31c21a207b
daniel-leibold <daniel_leibold@web.de>
2024-07-23 17:14:12 +0200

    amend latest position

--- create_pdf.sh
+++ create_pdf.sh
@@ -1,9 +1,11 @@
 # define themes kendall, elegant, stackoverflow
-THEMES=(elegant kendall stackoverflow)
+THEMES=(elegant kendall stackoverflow onepage-plus)
+echo "Starting to create resume pdf files..."
 # iterate over all themes
 for i in "${THEMES[@]}"
 do
-  resume export resume_$i.pdf --format pdf --theme $i
-  echo "created resume at resume_$i.pdf"
+  echo -n "File resume_dleibold_$i.pdf..."
+  resume export resume_dleibold_$i.pdf --format pdf --theme $i
+  echo "created!"
 done
 cp -v *.pdf /mnt/c/temp/
